fed43e1e486489480cc87b028190909b574ea63c
Tiago Madeira <tmadeira@gmail.com>
2013-09-14 16:51:55 -0300

    papi na parte 2

--- parte-2/parte-2.c
+++ parte-2/parte-2.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <hwloc.h>
+#include <papi.h>
 
 int main() {
     int level, numlevels;
-    hwloc_uint64_t cache[3];
-    unsigned line[3];
+
+    /* Variaveis das medicoes reais (hwloc) */
+    hwloc_uint64_t real_cache[3];
+    unsigned real_line[3];
     hwloc_topology_t topology;
     hwloc_obj_t obj;
+
+    /* Variaveis das medicoes estimadas (PAPI) */
+    int events[3] = { PAPI_L1_DCM, PAPI_L2_DCM, PAPI_L3_DCM };
+    long long misses[3];
+    int papilevels = 3;
     
     /* Inicializa hwloc */
     if (hwloc_topology_init(&topology)) {
+        fprintf(stderr, "Erro em hwloc_topology_init\n");
         exit(1);
     }
     hwloc_topology_load(topology);
@@ -19,12 +28,33 @@ int main() {
     level = 0;
     for (obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_PU, 0); obj; obj = obj->parent) {
         if (obj->type == HWLOC_OBJ_CACHE) {
-            cache[level] = obj->attr->cache.size;
-            line[level] = obj->attr->cache.linesize;
+            real_cache[level] = obj->attr->cache.size;
+            real_line[level] = obj->attr->cache.linesize;
             level++;
         }
     }
     numlevels = level;
+
+    /* Comeca a contar eventos */
+    while (PAPI_start_counters(events, papilevels) != PAPI_OK) {
+        papilevels--;
+    }
+
+    /* TODO: alguma coisa para estimar */
+
+    /* Le os contadores */
+    if (PAPI_read_counters(misses, papilevels) != PAPI_OK) {
+        fprintf(stderr, "Erro em PAPI_read_counters\n");
+        exit(1);
+    }
+
+    /* XXX */
+    printf("== DEBUG ==\n");
+    printf("L1 cache miss: %lld\n", misses[0]);
+    printf("L2 cache miss: %lld\n", misses[1]);
+    printf("L3 cache miss: %lld\n", misses[2]);
+    printf("\n");
+    /* /XXX */
 
     /* Imprime tabela */
     printf("Nível Linha Real Cache Real Linha Est. Cache Est.\n");
@@ -32,8 +62,15 @@ int main() {
     for (level = 0; level < 3; level++) {
         printf("%5d ", level+1);
         if (level < numlevels) {
-            printf("%10u ", line[level]);
-            printf("%9lluK ", (unsigned long long) cache[level] / 1024);
+            printf("%10u ", real_line[level]);
+            printf("%9lluK ", (unsigned long long) real_cache[level] / 1024);
+            if (level < papilevels) {
+                /* TODO: imprimir valores estimados */
+                printf("%10u ", real_line[level]);
+                printf("%9lluK", (unsigned long long) real_cache[level] / 1024);
+            } else {
+                printf("       N/D        N/D");
+            }
         } else {
             printf("       N/D        N/D        N/D        N/D");
         }
