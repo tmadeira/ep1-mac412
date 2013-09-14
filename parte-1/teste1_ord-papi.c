7275a122ed7906af88d35b22ffef64c28285ddf0
Tiago Madeira <tmadeira@gmail.com>
2013-09-14 15:28:53 -0300

    inicio

---
+++ parte-1/teste1_ord-papi.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <inttypes.h>
+#include <sys/time.h>
+
+#include "papi.h"
+
+#define SIZE 1000000
+
+static uint64_t residual_error = 0;
+
+inline uint64_t get_time(void) {
+    return (uint64_t) PAPI_get_virt_cyc();
+}
+
+void init_time(void) {
+	uint64_t t1, t2;
+	t1 = get_time();
+	t2 = get_time();
+	residual_error = t2 - t1;
+}
+
+inline uint64_t diff_time(uint64_t t1, uint64_t t2) {
+	return t2 - t1 - residual_error;
+}
+
+int compare_ints (const void *a, const void *b) {
+   const int *ia = (const int *) a;
+   const int *ib = (const int *) b; 
+   return (*ia > *ib) - (*ia < *ib);
+}
+
+int main (int argc, char *argv[]) {
+	int i, count;
+	int *array = (int*) malloc (SIZE * sizeof(int));
+	uint64_t start, end;
+
+    if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT) {
+        exit(1);
+    }
+
+	//Initialization
+	count = 0;
+	srand(time(NULL));
+	init_time();
+	for (i = 0; i < SIZE; i++)
+		array[i] = rand();
+
+	
+	// Uncomment this line to 
+	qsort(array, SIZE, sizeof (int), compare_ints);
+
+
+	//Measurement
+	start = get_time();
+	/*
+	 * É possível, em um vetor ordenado, fazer a contagem 
+	 * em tempo O(lg(n)) em vez de O(n) utilizando busca 
+	 * binária. Embora isto diminua sensivelmente o tempo 
+	 * este não é o intuito deste exercício.
+	 *
+	 */
+	for (i = 0; i < SIZE; i++)
+		if (array[i] < RAND_MAX / 2)
+			count++;
+	end = get_time();
+	uint64_t exec_time = diff_time(start, end);
+
+	printf("Time: %" PRIu64 " Count %d\n",  exec_time, count);
+	free(array);
+	return 0;
+}
